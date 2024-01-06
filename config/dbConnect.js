dfd75f556cb0d017a93d31b09e06025c900b902c
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2024-01-06 04:06:02 +0100

    Fixed minor issue for deployment

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose").default;
+
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://localhost:27017/Expense-Managment-App-Database",
-    );
-    console.log(`Server is running on ${mongoose.connection.host}`);
+    await mongoose.connect("mongodb://localhost:27017/Expense-Managment-App-Database");
+    console.log(`Connected to MongoDB on ${mongoose.connection.host}`);
   } catch (exception) {
     console.error(exception);
   }
--- index.js
+++ index.js
@@ -5,33 +5,32 @@ const dotenv = require("dotenv");
 const dbConnect = require("./config/dbConnect");
 const path = require("path");
 
-// Файл конфігу dot env;
+// Файл конфигурации dot env;
 dotenv.config();
-
-//База даних
-dbConnect();
 
 const app = express();
 
-//Мідлвари
+// Мидлвари
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-//Маршрутизація
+// Маршрутизация
 app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transactions", require("./routes/transactionRoute"));
 
-//Статичний файл
+// Статичный файл
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//Порт
+// Порт
 const PORT = 8080 || process.env.PORT;
 
-//Прослуховування серверу
-app.listen(PORT, () => {
-  console.log(`Server running successfully on ${PORT} port`);
+// Подключение к базе данных и прослушивание сервера
+dbConnect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running successfully on ${PORT} port`);
+  });
 });
